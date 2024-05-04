659272fcfbc8448c6197aa426ddd8e084fc36222
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-05-03 21:24:52 -0500

    Initial commit for project: FacturacionPracticeProject

---
+++ 03-FacturacionPracticeProject/springboot-facturacion-dependency-injection/src/main/java/com/manuelvl/curso/springboot/app/springbootfacturaciondependencyinjection/SpringbootFacturacionDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.manuelvl.curso.springboot.app.springbootfacturaciondependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFacturacionDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFacturacionDependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ 03-FacturacionPracticeProject/springboot-facturacion-dependency-injection/src/test/java/com/manuelvl/curso/springboot/app/springbootfacturaciondependencyinjection/SpringbootFacturacionDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelvl.curso.springboot.app.springbootfacturaciondependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFacturacionDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
