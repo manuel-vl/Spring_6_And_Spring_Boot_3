65f5181c204718759960e67e045a5553734f6193
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-05-20 11:07:53 -0500

    Initial commit for project: AspectOrientedProgramming

---
+++ 07-AspectOrientedProgramming(AoP)/programacion-orientada-aspectos/src/main/java/com/manuelvl/curso/springboot/app/programacionorientadaaspectos/ProgramacionOrientadaAspectosApplication.java
@@ -0,0 +1,13 @@
+package com.manuelvl.curso.springboot.app.programacionorientadaaspectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionOrientadaAspectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacionOrientadaAspectosApplication.class, args);
+	}
+
+}
---
+++ 07-AspectOrientedProgramming(AoP)/programacion-orientada-aspectos/src/test/java/com/manuelvl/curso/springboot/app/programacionorientadaaspectos/ProgramacionOrientadaAspectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelvl.curso.springboot.app.programacionorientadaaspectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionOrientadaAspectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
